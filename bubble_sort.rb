9856ca96f609ce722b300a3ad8e7c5fc0b21070a
kiwiofdoom1337 <186161940+kiwiofdoom1337@users.noreply.github.com>
2025-08-29 12:39:41 +0300

    add logic

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  loop do
+    change_count = 0
+    array.each_with_index do |num, pos|
+      if array[pos + 1] == nil
+        break
+      end
+      if (num <=> array[pos + 1]) > 0
+        temp_cont = num
+        array[pos] = array[pos + 1]
+        array[pos + 1] = temp_cont
+        change_count += 1
+      end
+    end
+    break if change_count == 0
+  end  
+  p array
+end
+
+bubble_sort([4,3,78,2,0,2])
